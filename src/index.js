073f6dfdeb1cd3855d5df8049646941fd8f989c6
泰志1020 <70305895+Taishi1020@users.noreply.github.com>
2021-08-19 17:26:05 +0900

    contの配列 constを用いた配列(初級編)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 //再宣言　上書き負荷
-const val3 = "const変数";
-console.log(val3);
+//const val3 = "const変数";
+//console.log(val3);
 
 //const変数を上書き
-val3 = "constは変数を上書きできません"; //上書きをしようとすると reatd-only（上書きできません）のエラーが出力される
+//val3 = "constは変数を上書きできません"; //上書きをしようとすると reatd-only（上書きできません）のエラーが出力される
+
+//const val3 = "const再宣言"; //　再宣言出来ないのに再宣言しようとしてるときのエラー  Identifier 'val3' has already been declared (9:6)
+
+//constで定義したオブジェクトはプロパティの経校が可能
+const val5 = ["doc", "cat"];
+val5[0] = "maus";
+val5.push("monkey");
+val5[2] = "sneake";
+console.log(val5);
